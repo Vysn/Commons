1c22f7a33b2115a47ea72ba38bef2203348ee5bf
Yucked <yucked@protonmail.com>
2020-07-07 22:17:12 -0400

    Moved extensions to Extensions dir and added JsonExtensions.cs

---
+++ src/Extensions/JsonExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Vysn.Commons.Extensions {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class JsonExtensions {
+        internal static bool TryRead(this ref Utf8JsonReader reader, string content) {
+            return reader.TokenType == JsonTokenType.PropertyName && reader.ValueTextEquals(content) && reader.Read();
+        }
+    }
+}
---
+++ src/Extensions/TaskExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vysn.Commons.Extensions {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class TaskExtensions {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="TaskCanceledException"></exception>
+        public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken) {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+            await using (cancellationToken.Register(obj => { }, tcs)) {
+                var resultTask = await Task.WhenAny(task, tcs.Task);
+                if (resultTask == tcs.Task) {
+                    throw new TaskCanceledException("Task was canceled.");
+                }
+
+                return await task;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="timeout"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="TaskCanceledException"></exception>
+        public static async Task<T> CancelAfter<T>(this Task<T> task, TimeSpan timeout) {
+            using var cts = new CancellationTokenSource();
+            var delayTask = Task.Delay(timeout, cts.Token);
+            var resultTask = Task.WhenAny(task, delayTask);
+
+            if (resultTask == delayTask) {
+                throw new TaskCanceledException("Task was canceled.");
+            }
+
+            cts.Cancel(false);
+            return await task;
+        }
+    }
+}
