f86551d25cdbc77e7dc5b227e7fe11e3d62bf752
Yucked <yucked@protonmail.com>
2020-07-12 12:52:36 -0400

    Added implicit Snowflake operator.

--- src/Snowflake.cs
+++ src/Snowflake.cs
@@ -1,107 +1,119 @@
 ﻿namespace Vysn.Commons {
-	/// <summary>
-	/// 
-	/// </summary>
-	public readonly struct Snowflake {
-		private readonly ulong _value;
+    /// <summary>
+    /// 
+    /// </summary>
+    public readonly struct Snowflake {
+        private readonly ulong _value;
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="value"></param>
-		public Snowflake(ulong value) {
-			_value = value;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public Snowflake(ulong value) {
+            _value = value;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
-		public static implicit operator ulong(in Snowflake value) {
-			return value._value;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator ulong(in Snowflake value) {
+            return value._value;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="snowflake"></param>
-		/// <param name="other"></param>
-		/// <returns></returns>
-		public static bool operator ==(Snowflake snowflake, Snowflake other) {
-			return snowflake._value == other._value;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator Snowflake(in ulong value) {
+            return new Snowflake(value);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="snowflake"></param>
-		/// <param name="other"></param>
-		/// <returns></returns>
-		public static bool operator !=(Snowflake snowflake, Snowflake other) {
-			return !(snowflake == other);
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="snowflake"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool operator ==(Snowflake snowflake, Snowflake other) {
+            return snowflake._value == other._value;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="snowflake"></param>
-		/// <param name="other"></param>
-		/// <returns></returns>
-		public static bool operator ==(Snowflake snowflake, ulong other) {
-			return snowflake._value == other;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="snowflake"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool operator !=(Snowflake snowflake, Snowflake other) {
+            return !(snowflake == other);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="snowflake"></param>
-		/// <param name="other"></param>
-		/// <returns></returns>
-		public static bool operator !=(Snowflake snowflake, ulong other) {
-			return snowflake._value != other;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="snowflake"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool operator ==(Snowflake snowflake, ulong other) {
+            return snowflake._value == other;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="value"></param>
-		/// <param name="snowflake"></param>
-		/// <returns></returns>
-		public static bool TryParse(string value, out Snowflake snowflake) {
-			if (!ulong.TryParse(value, out var result)) {
-				snowflake = default;
-				return false;
-			}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="snowflake"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool operator !=(Snowflake snowflake, ulong other) {
+            return snowflake._value != other;
+        }
 
-			snowflake = new Snowflake(result);
-			return true;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="snowflake"></param>
+        /// <returns></returns>
+        public static bool TryParse(string value, out Snowflake snowflake) {
+            if (!ulong.TryParse(value, out var result)) {
+                snowflake = default;
+                return false;
+            }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="other"></param>
-		/// <returns></returns>
-		public bool Equals(Snowflake other) {
-			return _value == other._value;
-		}
+            snowflake = new Snowflake(result);
+            return true;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="obj"></param>
-		/// <returns></returns>
-		public override bool Equals(object obj) {
-			return obj is Snowflake other && Equals(other);
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Snowflake other) {
+            return _value == other._value;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <returns></returns>
-		public override int GetHashCode() {
-			return _value.GetHashCode();
-		}
-	}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj) {
+            return obj switch {
+                ulong val           => val == _value,
+                Snowflake snowflake => Equals(snowflake)
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() {
+            return _value.GetHashCode();
+        }
+    }
 }
